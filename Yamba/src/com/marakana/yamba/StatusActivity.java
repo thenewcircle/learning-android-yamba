e59a689aa66e2a9dce8b629c0d86696412be4c5a
Marko Gargenta <marko@marakana.com>
2013-08-09 22:54:51 -0700

    Implemented simple onClick()

--- Yamba/src/com/marakana/yamba/StatusActivity.java
+++ Yamba/src/com/marakana/yamba/StatusActivity.java
@@ -1,23 +1,41 @@
 package com.marakana.yamba;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class StatusActivity extends Activity {
+public class StatusActivity extends Activity implements OnClickListener {
+	private static final String TAG = "StatusActivity";
+	private EditText editStatus;
+	private Button buttonTweet;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_status);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_status);
 
+		editStatus = (EditText) findViewById(R.id.editStatus);
+		buttonTweet = (Button) findViewById(R.id.buttonTweet);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.status, menu);
-        return true;
-    }
-    
+		buttonTweet.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View view) {
+		String status = editStatus.getText().toString();
+		Log.d(TAG, "onClicked with status: " + status);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.status, menu);
+		return true;
+	}
+
 }
