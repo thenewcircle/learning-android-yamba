b50c90e64749541848ed5116cf0681bd5c7f2d68
Marko Gargenta <marko@marakana.com>
2013-08-11 18:29:45 -0700

    Implemented landscape layout

--- Yamba/src/com/marakana/yamba/StatusActivity.java
+++ Yamba/src/com/marakana/yamba/StatusActivity.java
@@ -1,14 +1,18 @@
 package com.marakana.yamba;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.marakana.android.yamba.clientlib.YambaClient;
@@ -18,6 +22,8 @@ public class StatusActivity extends Activity implements OnClickListener {
 	private static final String TAG = "StatusActivity";
 	private EditText editStatus;
 	private Button buttonTweet;
+	private TextView textCount;
+	private int defaultTextColor;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +32,35 @@ public class StatusActivity extends Activity implements OnClickListener {
 
 		editStatus = (EditText) findViewById(R.id.editStatus);
 		buttonTweet = (Button) findViewById(R.id.buttonTweet);
+		textCount = (TextView) findViewById(R.id.textCount);
 
 		buttonTweet.setOnClickListener(this);
+
+		defaultTextColor = textCount.getTextColors().getDefaultColor();
+		editStatus.addTextChangedListener(new TextWatcher() {
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				int count = 140 - editStatus.length();
+				textCount.setText(Integer.toString(count));
+				textCount.setTextColor(Color.GREEN);
+				if (count < 10)
+					textCount.setTextColor(Color.RED);
+				else
+					textCount.setTextColor(defaultTextColor);
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
+			}
+
+		});
 	}
 
 	@Override
@@ -44,22 +77,23 @@ public class StatusActivity extends Activity implements OnClickListener {
 		protected String doInBackground(String... params) {
 			YambaClient yambaCloud = new YambaClient("student", "password");
 			try {
-				yambaCloud.postStatus( params[0] );
+				yambaCloud.postStatus(params[0]);
 				return "Successfully posted";
 			} catch (YambaClientException e) {
 				e.printStackTrace();
 				return "Failed to post to yamba service";
 			}
 		}
-		
+
 		@Override
 		protected void onPostExecute(String result) {
 			super.onPostExecute(result);
-			
-			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
+
+			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG)
+					.show();
 		}
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
