ed14074c66bb426e5b9139953bc45f14aee438cb
masmoo <mas@quietfury.org>
2013-08-25 02:30:00 -0400

    working source

--- Yamba/src/com/marakana/android/yamba/StatusFragmentWDetails.java
+++ Yamba/src/com/marakana/android/yamba/StatusFragmentWDetails.java
@@ -1,15 +1,17 @@
-package com.marakana.yamba;
+package com.marakana.android.yamba;
 
-import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
-import android.view.Menu;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,21 +20,25 @@ import android.widget.Toast;
 import com.marakana.android.yamba.clientlib.YambaClient;
 import com.marakana.android.yamba.clientlib.YambaClientException;
 
-public class StatusActivity extends Activity implements OnClickListener {
+public class StatusFragmentWDetails extends Fragment implements OnClickListener {
 	private static final String TAG = "StatusActivity";
 	private EditText editStatus;
 	private Button buttonTweet;
 	private TextView textCount;
 	private int defaultTextColor;
+	
+	private String lastmsg;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_status);
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
 
-		editStatus = (EditText) findViewById(R.id.editStatus);
-		buttonTweet = (Button) findViewById(R.id.buttonTweet);
-		textCount = (TextView) findViewById(R.id.textCount);
+		View view = inflater
+				.inflate(R.layout.fragment_status, container, false);
+		
+		editStatus = (EditText) view.findViewById(R.id.editStatus);
+		buttonTweet = (Button) view.findViewById(R.id.buttonTweet);
+		textCount = (TextView) view.findViewById(R.id.textCount);
 
 		buttonTweet.setOnClickListener(this);
 
@@ -61,8 +67,10 @@ public class StatusActivity extends Activity implements OnClickListener {
 			}
 
 		});
-	}
 
+		return view;
+	}
+	
 	@Override
 	public void onClick(View view) {
 		String status = editStatus.getText().toString();
@@ -78,6 +86,7 @@ public class StatusActivity extends Activity implements OnClickListener {
 			YambaClient yambaCloud = new YambaClient("student", "password");
 			try {
 				yambaCloud.postStatus(params[0]);
+				lastmsg = params[0];
 				return "Successfully posted";
 			} catch (YambaClientException e) {
 				e.printStackTrace();
@@ -89,16 +98,30 @@ public class StatusActivity extends Activity implements OnClickListener {
 		protected void onPostExecute(String result) {
 			super.onPostExecute(result);
 
-			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG)
+			Toast.makeText(StatusFragmentWDetails.this.getActivity(), result, Toast.LENGTH_LONG)
 					.show();
+			
+			if(result.equals("Successfully posted")) {
+				showDetails();
+			}
 		}
 	}
+	
+	//--  The ShowDetails call.
+	public void showDetails() {
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.status, menu);
-		return true;
+		  //-- Attempt to find the details fragment
+		DetailsFragment fragment = (DetailsFragment) getFragmentManager()
+				.findFragmentById(R.id.fragment_details);
+
+		  //-- Is details fragment visible?
+		if (fragment != null && fragment.isVisible()) {
+			  //-- update the view
+			fragment.updateView("You", lastmsg);
+		} else {
+			  //-- start up the view and pass the relevant info
+			startActivity(new Intent(getActivity(), DetailsActivity.class)
+					.putExtra("user", "You").putExtra("msg",lastmsg));
+		}
 	}
-
 }
